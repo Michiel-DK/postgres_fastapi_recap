5e17a0823d4a4b98a1b63687da4bfbed6204bcab
Michiel De Koninck <michieldekoninck2@gmail.com>
2023-02-24 16:31:38 +0900

    working script - great succes

--- setup_tables.sql
+++ setup_tables.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE airbnb;
-
-
+\c airbnb
 CREATE TABLE cities (
   id INT PRIMARY KEY,
   city TEXT,
@@ -9,3 +8,5 @@ CREATE TABLE cities (
   bedrooms INT,
   satisfaction REAL
 );
+\copy cities FROM 'merged.csv' DELIMITER ',' CSV HEADER;
+\! rm merged.csv
